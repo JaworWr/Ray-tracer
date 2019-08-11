eb27e5cc2ca1539c3b1c6a5eca8d0ad1e3362df0
Michał Jaworski <jawor403@gmail.com>
2019-08-11 16:17:36 +0200

    Add comments to SceneValidator.hs

--- SceneValidator.hs
+++ SceneValidator.hs
@@ -6,6 +6,7 @@ import Scene
 import Control.Monad.Except
 import Data.Char
 
+-- sprawdzenie czy scena jest poprawnie zdefiniowana
 validateScene :: (Show t, Color t) => Scene t -> Either String ()
 validateScene s = do
     validateMinValue "image width" (pxWidth s) 0
@@ -18,23 +19,28 @@ validateScene s = do
     mapM_ validateLightSource (enumerate $ lights s)
     mapM_ validateObject (enumerate $ objects s)
 
+-- przyporządkowanie wartościom indeksów (począwszy od 1)
 enumerate :: [a] -> [(Int, a)]
 enumerate = zip [1..]
 
+-- błąd informujący o przekroczeniu minimalnej wartości
 minValueStr :: Show t => String -> t -> String
 minValueStr vt m = capitalize vt ++ " must be greater than or equal to " ++ show m where
     capitalize [] = []
     capitalize (x:xs) = toUpper x : xs
 
+-- sprawdzenie czy minimalna wartość nie jest przekroczona
 validateMinValue :: (Show t, Ord t) => String -> t -> t -> Either String ()
 validateMinValue vt v m = when (v < m) $ throwError
     ("Invalid " ++ vt ++ " value: " ++ show v ++ ". " ++ minValueStr vt m)
 
+-- sprawdzenie czy kolor jest poprawnie zdefiniowany
 validateColor :: (Show t, Color t) => String -> t -> Either String ()
 validateColor ot c = unless (colorValid c) $ throwError invalidColorStr where
     invalidColorStr = "Invalid " ++ ot ++ " color: " ++ show c
         ++ ". " ++ minValueStr "all components" 0
 
+-- sprawdzenie czy źródło światła jest poprawnie zdefiniowane
 validateLightSource :: (Show t, Color t) => (Int, LightSource t) -> Either String ()
 validateLightSource (i, s) = validateColor "lightsource" (lightSourceColor s)
     `catchError` throwLSError
@@ -42,6 +48,7 @@ validateLightSource (i, s) = validateColor "lightsource" (lightSourceColor s)
         invalidLSErrorStr = "Lightsource #" ++ show i ++ ":\n"
         throwLSError = throwError . (invalidLSErrorStr ++)
 
+-- sprawdzenie czy obiekt sceny jest poprawnie zdefiniowany
 validateObject :: (Show t, Color t) => (Int, Object t) -> Either String ()
 validateObject (i, o) = do
     unless (geometryValid o) $
@@ -51,12 +58,15 @@ validateObject (i, o) = do
         invalidObjectStr = "Object #" ++ show i ++ ":\n"
         throwObjError = throwError . (invalidObjectStr ++)
 
+-- sprawdzenie czy powierzchnia jest poprawnie zdefiniowana
 validateSurface :: (Show t, Color t) => Surface t -> Either String ()
 validateSurface (Diffusive c) = validateColor "surface" c
 validateSurface Reflective = return ()
 validateSurface (Luminous c) = validateColor "surface" c
 validateSurface (Mixed ss) = mapM_ validateMixedSurfElem $ enumerate ss
 
+-- sprawdzenie czy powierzchnia wchodząca w skład powierzchni typu mieszanego
+-- jest poprawnie zdefiniowana
 validateMixedSurfElem :: (Show t, Color t) => (Int, (Double, Surface t)) -> Either String ()
 validateMixedSurfElem (i, (w, s)) = do
     validateMinValue "mixed surface component weight" w 0
