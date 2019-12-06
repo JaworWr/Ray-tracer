f961dba6c7a587626ad700773f1b564fdeb585e3
Michał Jaworski <jawor403@gmail.com>
2019-12-06 19:27:00 +0100

    Add option parsing

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, TupleSections #-}
 
 module Main where
 
@@ -6,13 +6,58 @@ import DataTypes
 import SceneParser
 import SceneValidator
 import SceneRenderer
+import Data.Either
 import qualified Data.ByteString as BStr
 import Control.Monad.Except
 import System.Environment
 import System.IO.Error
+import System.Console.GetOpt
 import Data.List
 import Codec.BMP
 import qualified Graphics.Gloss as G
+
+data Options = Options {
+    savePath :: Maybe String,
+    saveImg :: Bool,
+    showImg :: Bool,
+    showHelp :: Bool
+} deriving (Eq, Show)
+
+defaultOpts :: Options
+defaultOpts = Options {
+    savePath = Nothing,
+    saveImg = True,
+    showImg = False,
+    showHelp = False
+}
+
+options :: [OptDescr (Options -> Options)]
+options = [
+    Option ['n'] ["no-save"]
+        (NoArg $ \opt -> opt { saveImg = False })
+        "Do not save the image",
+    Option ['o'] ["output-name"]
+        (ReqArg (\s opt -> opt { savePath = Just s }) "name")
+        "Output file name",
+    Option ['s'] ["show"]
+        (NoArg $ \opt -> opt { showImg = True })
+        "Show the image after rendering",
+    Option ['h'] ["help"]
+        (NoArg $ \opt -> opt { showHelp = True })
+        "Show this help message"
+    ]
+
+usageStr :: String -> String
+usageStr progName = usageInfo header options where
+    header = "Usage: " ++ progName ++ " [OPTION...] file"
+
+parseOptions :: [String] -> Either String (Options, Maybe String)
+parseOptions args = (opts, ) <$> path where
+    (fs, rs, errs) = getOpt Permute options args
+    opts = foldl (flip id) defaultOpts fs
+    path = case errs of
+        [] -> return $ fst <$> uncons rs
+        _ -> throwError $ concat errs ++ "\n"
 
 -- funkcja przekształcająca obraz do formatu BMP
 imageToBmp :: Color t => Image t -> BMP
@@ -35,14 +80,25 @@ showImage name i = do
     G.display (G.InWindow name (imWidth i, imHeight i) (0, 0))
         G.black (G.bitmapOfBMP bmp)
 
+-- pomocnicza funkcja przekształcająca typ Maybe na instancję MonadError
+maybeThrow :: MonadError e m => e -> Maybe a -> m a
+maybeThrow e = maybe (throwError e) return
+
 runRayTracer :: ExceptT String IO ()
 runRayTracer = do
-    args <- (fmap fst . uncons) <$> lift getArgs
-    path <- maybe (throwError "No input file specified") return args
-    s <- withExceptT show . ExceptT . tryIOError . readFile $ path
-    scene <- withExceptT show . liftEither $ parseScene path s
-    validated <- liftEither $ validateScene scene
-    lift . showImage path . renderValidated $ validated
+    progName <- lift getProgName
+    (opts, path) <- withExceptT (++ usageStr progName) $
+        lift getArgs >>= liftEither . parseOptions
+    if showHelp opts
+        then lift . putStrLn $ usageStr progName
+        else do
+            path <- maybeThrow
+                ("No input file specified.\n\n" ++ usageStr progName)
+                path
+            s <- withExceptT show . ExceptT . tryIOError . readFile $ path
+            scene <- withExceptT show . liftEither $ parseScene path s
+            validated <- liftEither $ validateScene scene
+            lift . showImage path . renderValidated $ validated
 
 main :: IO ()
 main = runExceptT runRayTracer >>= \case
